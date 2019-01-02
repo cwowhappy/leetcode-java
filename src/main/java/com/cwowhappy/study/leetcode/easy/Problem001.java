0b2378ecc4bfef0c426b4c4ddef5910638a9bae9
李晓毅 <lixiaoyi03@meituan.com>
2019-01-02 16:40:55 +0800

    save

---
+++ src/main/java/com/cwowhappy/study/leetcode/easy/Problem001.java
@@ -0,0 +1,14 @@
+package com.cwowhappy.study.leetcode.easy;
+
+public class Problem001 {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+
+    public static class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            return new int[]{0};
+        }
+    }
+}
