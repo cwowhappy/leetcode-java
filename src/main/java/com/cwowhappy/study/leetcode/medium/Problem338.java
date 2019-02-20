2bbd5335a7483299fc96699090b7423675cab8e0
李晓毅 <lixiaoyi03@meituan.com>
2019-02-20 22:53:44 +0800

    save

---
+++ src/main/java/com/cwowhappy/study/leetcode/medium/Problem338.java
@@ -0,0 +1,29 @@
+package com.cwowhappy.study.leetcode.medium;
+
+public class Problem338 {
+    public static class Solution {
+        public int[] countBits(int num) {
+            int[] sizes = new int[num + 1];
+            int nearestExp = 1;
+            if (0 == num) {
+                sizes[0] = 0;
+            } else if (1 == num) {
+                sizes[0] = 0;
+                sizes[1] = 1;
+            } else {
+                sizes[0] = 0;
+                sizes[1] = 1;
+                for (int i = 2; i <= num; i ++) {
+                    int tmp = i - nearestExp;
+                    if (tmp == nearestExp) {
+                        nearestExp = i;
+                        sizes[i] = 1;
+                    } else {
+                        sizes[i] = 1 + sizes[tmp];
+                    }
+                }
+            }
+            return sizes;
+        }
+    }
+}
---
+++ src/main/java/com/cwowhappy/study/leetcode/medium/Problem877.java
@@ -0,0 +1,9 @@
+package com.cwowhappy.study.leetcode.medium;
+
+public class Problem877 {
+    public static class Solution {
+        public boolean stoneGame(int[] piles) {
+
+        }
+    }
+}
